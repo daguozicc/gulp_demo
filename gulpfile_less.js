1ffd04230d75a35cbd8e2d69e40e1be5802ebee1
大果子Cc <yzcxiaozhi@126.com>
2018-07-07 16:12:01 +0800

    less文件增加注释

--- gulpfile_less.js
+++ gulpfile_less.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+// 创建一个gulp任务
 gulp.task('less',function(){
+  // 查找文件
   gulp.src('less/**.less')
+  // 执行less文件
   .pipe(less())
+  // 输出css文件
   .pipe(gulp.dest('dless/css'))
 });
-
+// 监听less文件的改动
 gulp.task('auto',function(){
   gulp.watch('less/**.less',['less'])
 })
